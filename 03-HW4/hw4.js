46caec0aec1c528560e21aadec68e0c0cd10a7ab
TinaKova <tmp.itstep@gmail.com>
2022-03-10 20:14:15 +0200

    HW4.

--- 03-HW4/hw4.js
+++ 03-HW4/hw4.js
@@ -2,48 +2,38 @@
 const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"];
 const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"];
 const marks = [4, 5, 5, 3, 4, 5];
-const pairsOfStudents=getPairsArray(students);
-const  pairsWithThemes=addThemesToPair(themes,pairsOfStudents);
+
+const pairs=getPairsArray(students);
+console.log(pairs);
+const  pairsWithThemes=addThemesToPair(themes,pairs);
+console.log(pairsWithThemes);
+const studentsWithMarks=giveMarkToStudent(students,marks);
+console.log(studentsWithMarks);
+const pairsWithRandomMarks=giveRandomMarkToPairsOfStudents(pairsWithThemes);
+console.log(pairsWithRandomMarks);
 
 function getPairsArray(arrStudents) {
     let pairs = [];
-    let newArr = arrStudents.slice()
-    for (let i = 0; i < newArr.length; i++) {
-        if (!newArr[i].endsWith('а')) {
-            let man = newArr.splice(i, 1);
+    let cloneArrStudents = [].concat(arrStudents);
+    for (let i = 0; i < cloneArrStudents.length; i++) {
+        if (!cloneArrStudents[i].endsWith('а')) {
+            let man = cloneArrStudents.splice(i, 1);
             pairs.push(man);
             i--;
         }
     }
-    for (let i = 0; i < pairs.length; i++) {
-        pairs[i].push(newArr[i]);
-    }
-
+    pairs.forEach((item, index)=>item.push(cloneArrStudents[index]));
     return pairs;
 }
-
-console.log(pairsOfStudents);
-console.log(students);
 
 function addThemesToPair(arrThemes, arrPairs){
     return arrPairs.map((item,index)=>[item.join(' і '),themes[index]]);
 }
 
-console.log(pairsWithThemes);
-console.log(students);
-
 function giveMarkToStudent(arrStudent, arrMarks){
     return arrStudent.map((item, index)=>[item,arrMarks[index]])
 }
-console.log(giveMarkToStudent(students,marks));
-console.log(students);
 
 function giveRandomMarkToPairsOfStudents(arrPairs){
-    let newArr=[];
-    arrPairs.forEach(item=>{
-        newArr.push(item.concat(Math.floor(Math.random()*(5-1)+1)));
-    })
-return newArr;
+  return arrPairs.map(item=>item.concat(Math.floor(Math.random()*(5-1)+1)));
 }
-console.log(giveRandomMarkToPairsOfStudents(pairsWithThemes));
-console.log(pairsWithThemes);
