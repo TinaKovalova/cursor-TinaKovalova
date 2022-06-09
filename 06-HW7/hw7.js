0d9c45511bd5f4145e13a2d360558e0848031a2f
Tina Kovalova <kovalevatkv@gmail.com>
2022-06-10 01:21:54 +0300

    HW7

--- 06-HW7/hw7.js
+++ 06-HW7/hw7.js
@@ -16,12 +16,12 @@ function getTotalTaxes() {
 }
 
 function getMySalary() {
-    const result = {}
+    const result = {};
     setInterval(function () {
         result.salary = Number((Math.random() * (2000 - 1500) + 1500).toFixed(2));
         result.taxes = getMyTaxes.call(this, result.salary);
         result.profit = Number((result.salary - result.taxes).toFixed(2));
-        console.log(result)
+        console.log(result);
     }.bind(this), 10000);
 }
 
