4e0f67be6da98fbcf1b22bcd1a59982d42ee129a
Tina Kovalova <kovalevatkv@gmail.com>
2022-06-13 19:06:14 +0300

    HW8.1

--- 07-HW8/hw8.js
+++ 07-HW8/hw8.js
@@ -2,7 +2,6 @@
 
 const student = new Student('Вища Школа Психотерапії м.Одеса', 2, 'Остап Бендер');
 console.log('student.getInfo() => ', student.getInfo());
-// console.log(student.marks = 4);
 [5, 3, 4].forEach(mark => student.marks = mark)
 console.log('student.marks => ', student.marks);
 console.log('student.getAverageMark() => ', student.getAverageMark());
@@ -11,17 +10,13 @@ console.log('student.marks after student.dismiss() => ', student.marks);
 student.recover();
 console.log('student.marks after student.recover() => ', student.marks);
 
-
 const budgetStudent = new BudgetStudent('Вища Школа', 1, 'Семен Гуля');
 
 console.log('budgetStudent.getInfo() => ', budgetStudent.getInfo());
-// budgetStudent.marks = 4;
-// budgetStudent.marks = 5;
-// budgetStudent.marks = 4;
 [4, 5, 4].forEach(mark => budgetStudent.marks = mark)
 console.log('budgetStudent.marks => ', budgetStudent.marks);
 console.log('budgetStudent.getAverageMark() => ', budgetStudent.getAverageMark());
-// budgetStudent.dismiss();
-// console.log('budgetStudent.marks after budgetStudent.dismiss() => ', budgetStudent.marks);
-// budgetStudent.recover();
-// console.log('budgetStudent.marks after budgetStudent.recover() => ', budgetStudent.marks);
+budgetStudent.dismiss();
+console.log('budgetStudent.marks after budgetStudent.dismiss() => ', budgetStudent.marks);
+budgetStudent.recover();
+console.log('budgetStudent.marks after budgetStudent.recover() => ', budgetStudent.marks);
