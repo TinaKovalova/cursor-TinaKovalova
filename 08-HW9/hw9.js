1b241d0f663e392adb48a4bcf6765a4db7c8ace1
Tina Kovalova <kovalevatkv@gmail.com>
2022-06-15 20:09:31 +0300

    HW9

---
+++ 08-HW9/hw9.js
@@ -0,0 +1,22 @@
+'use strict';
+const body = document.querySelector('body');
+
+function getRandomColor() {
+    const random = () => Math.floor(Math.random() * 256);
+    return `rgb(${random()}, ${random()}, ${random()})`;
+}
+
+function generateBlocksInterval() {
+    for (let i = 0; i < 5; i++) {
+        const row=document.createElement('div');
+        row.classList.add('container');
+        body.append(row);
+        for (let j = 0; j < 5; j++) {
+            const block = document.createElement('div');
+            block.classList.add('block');
+            setInterval(()=>block.style.backgroundColor=getRandomColor(),1000);
+            row.append(block);
+        }
+    }
+}
+generateBlocksInterval();
