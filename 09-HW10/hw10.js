6bf7bb3905e21ec208d6edc10f811dd8c06441db
Tina Kovalova <kovalevatkv@gmail.com>
2022-06-19 16:38:05 +0300

    HW10.0

--- 09-HW10/hw10.js
+++ 09-HW10/hw10.js
@@ -3,21 +3,20 @@
 const body = document.querySelector('body');
 const container = document.querySelector('.container');
 const sounds = {
-    KeyA: 'audio/keyA.mp3',
-    KeyS: '',
-    KeyD: '',
-    KeyF: '',
-    KeyG: '',
-    KeyH: '',
-    KeyJ: '',
-    KeyK: '',
-    KeyL: ''
+    KeyA: 'sounds/drumsCongaSingleHit.mp3',
+    KeyS: 'sounds/drumsTomTomDoubleHit.mp3',
+    KeyD: 'sounds/drumsTomTomSingleHit.mp3',
+    KeyF: 'sounds/snareDrumSingleHit.mp3',
+    KeyG: 'sounds/cymbalBellCrashSoundEffect.mp3',
+    KeyH: 'sounds/cymbalSmallCrash.mp3',
+    KeyJ: 'sounds/bassDrumDoubleKick.mp3',
+    KeyK: 'sounds/bassDrumSingleHit.mp3',
+    KeyL: 'sounds/triangleSingleHit.mp3'
 };
 let currentSoundBlock = null;
 
 function playSound(event) {
-    const key = event.type == 'keydown' && event.code in sounds ? event.code : event.target.id;
-    console.log({key})
+    const key = event.type == 'keydown' && event.code in sounds ? event.code : event.target.id || event.target.parentElement.id;
     if (key) {
         currentSoundBlock?.classList.toggle('key-sound-selected');
         const selectedSoundBlock = document.querySelector(`#${key}`);
