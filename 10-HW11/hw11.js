89f56e51eef7c4740cb12bb95c4eee14797ce4e9
Tina Kovalova <kovalevatkv@gmail.com>
2022-06-25 20:10:22 +0300

    HW11

---
+++ 10-HW11/hw11.js
@@ -0,0 +1,19 @@
+'use strict';
+
+async function getRandomChinese(length) {
+    if (length <= 0) return;
+    let chinese = '';
+    while (length != 0) {
+        chinese += await new Promise(resolve => {
+            setTimeout(() => {
+                const sign = Date.now().toString().slice(-5);
+                const char = String.fromCharCode(+sign);
+                resolve(char)
+            }, 50)
+        });
+        length--;
+    }
+    return chinese;
+}
+
+getRandomChinese(4).then(result => console.log(result))
